2b90430a57fafec22e3459faa03730e4955afaab
Usman868 <usman03228270254@gmail.com>
2026-07-08 15:39:19 +0500

    added blog , comment models all controllers and all route file

---
+++ server/controllers/blogController.js
@@ -0,0 +1,124 @@
+import fs from "fs";
+import { toFile } from "@imagekit/nodejs";
+import Blog from "../models/Blog.js";
+import Comment from "../models/Comment.js";
+import imagekit from "../configs/imageKit.js";
+
+export const addBlog = async (req, res) => {
+  try {
+    const { title, subTitle, description, category, isPublished } = JSON.parse(
+      req.body.blog,
+    );
+    const imagefile = req.file;
+    // Check if all fields are present
+    if (!title || !subTitle || !category || !imagefile) {
+      return res.json({ success: false, message: "Missing required fields" });
+    }
+
+    const response = await imagekit.files.upload({
+      file: await toFile(imagefile.buffer, imagefile.originalname),
+      fileName: imagefile.originalname,
+      folder: "/blogs",
+    });
+
+    const transformedUrl = imagekit.helper.buildSrc({
+      src: response.url, // Use 'src' in the new SDK
+      transformation: [
+        {
+          width: "1200",
+          quality: "auto",
+          format: "webp",
+        },
+      ],
+    });
+
+    const image = transformedUrl;
+
+    await Blog.create({
+      title,
+      subTitle,
+      description,
+      category,
+      image,
+      isPublished,
+    });
+
+    res.json({ success: true, message: "Blog added successfully" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getAllBlog = async (req, res) => {
+  try {
+    const blogs = await Blog.find({ isPublished: true });
+    res.json({ success: true, blogs });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getBlogById = async (req, res) => {
+  try {
+    const { blogId } = req.params;
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+      res.json({ success: false, message: "Blog not found" });
+      return;
+    }
+    res.json({ success: true, blog });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const deleteBlogById = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const blog = await Blog.findByIdAndDelete(id);
+    //  Delete all comments associated  with the blog
+    await Comment.deleteMany({ blog: id });
+
+    res.json({ success: true, message: "Blog deleted successfully" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const togglePublish = async (req, res) => {
+  try {
+    const { id } = req.body;
+    const blog = await Blog.findByIdAndUpdate(id);
+    if (!blog) {
+      return res.json({ success: false, message: "Blog not found" });
+    }
+    blog.isPublished = !blog.isPublished;
+    await blog.save();
+    res.json({ success: true, message: "Blog status updated" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const addComment = async (req, res) => {
+  try {
+    const { blog, name, content } = req.body;
+    await Comment.create({ blog, name, content });
+    res.json({ success: true, message: "Comment added for review" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const getBlogComments = async (req, res) => {
+  try {
+    const { blogId } = req.body;
+    const comments = await Comment.find({
+      blog: blogId,
+      isApproved: true,
+    }).sort({ createdAt: -1 });
+    res.json({ success: true, comments });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
---
+++ server/routes/blogRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  addBlog,
+  addComment,
+  deleteBlogById,
+  getAllBlog,
+  getBlogById,
+  getBlogComments,
+  togglePublish,
+} from "../controllers/BlogController.js";
+import upload from "../middleware/multer.js";
+import auth from "../middleware/auth.js";
+
+const blogRouter = express.Router();
+
+blogRouter.post("/add", auth, upload.single("image"), addBlog);
+blogRouter.get("/all", getAllBlog);
+blogRouter.get("/:blogId", getBlogById);
+blogRouter.post("/delete/", auth, deleteBlogById);
+blogRouter.post("/toggle-publish", auth, togglePublish);
+
+
+blogRouter.post("/add-comment", addComment);
+blogRouter.post("/comments", getBlogComments);
+
+
+export default blogRouter;
